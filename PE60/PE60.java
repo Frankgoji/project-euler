382e3ad4cf5de6c45c7bcff94023f82b47a73849
Franklin Lee <frankgoji@gmail.com>
2015-07-30 12:41:52 -0700

    Commit on Thu Jul 30 12:41:52 PDT 2015

--- PE60/PE60.java
+++ PE60/PE60.java
@@ -24,12 +24,24 @@ import java.util.List;
 /** Prime pair sets. */
 public class PE60 {
 
+    public static ArrayList<BigInteger> primes;
+
     /** Checks if BigIntegers A and B are prime pairs. */
     public static boolean isPrimePair(BigInteger a, BigInteger b) {
-        return false;
+        return isPrime(new BigInteger(a.toString() + b.toString())) &&
+            isPrime(new BigInteger(b.toString() + a.toString()));
     }
 
     /** Main program. */
 	public static void main(String[] args) {
+        Path primePath = FileSystems.getDefault().getPath("PE60/primes.txt");
+        List<String> x = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(primePath,
+                    StandardCharsets.UTF_8)) {
+            x = Arrays.asList(reader.readLine().split(","));
+        } catch (IOException i) {
+            System.out.println("IOException");
+            System.exit(1);
+        }
 	}
 }
